677cc51771087e30a71fcece5580c2191d13d34c
Wendy Xu <guangw.xu@gmail.com>
2017-01-29 22:41:10 -0500

    Done.

--- components/App.js
+++ components/App.js
@@ -9,27 +9,54 @@ const fileStore = require('../stores/fileStore');
 const actions = require('../actions');
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      files: fileStore.getState(),
+      selectedFileIndex: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+
+
   componentDidMount() {
     // TODO
+    this.removeListener = fileStore.addListener((files) => {
+      this.setState({files});
+    }); 
   }
+
   componentWillUnmount() {
     // TODO
+    this.removeListener();
   }
+
   handleChange(ev) {
     const { selectedFileIndex } = this.state;
     // TODO Dispatch action
+    actions.updateFile(selectedFileIndex, ev.target.value);
   }
+
   handleSelect(selectedFileIndex) {
     // TODO Update selectedFileIndex state
+    this.setState({selectedFileIndex: selectedFileIndex}); 
   }
+
   handleAdd(ev) {
     ev.preventDefault();
     // TODO Dispatch action
+    actions.addFile();
   }
+
   handleRemove(ev) {
     ev.preventDefault()
     // TODO Dispatch action
+    actions.removeFile(this.state.selectedFileIndex);
   }
+
   render() {
     const { files, selectedFileIndex } = this.state;
     const file = files[selectedFileIndex];
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,13 +4,24 @@ const React = require('react');
 const SidebarItem = require('./SidebarItem');
 
 class Sidebar extends React.Component {
-  handleClick(index, ev) {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(index, ev) {
+    ev.preventDefault();
+    this.props.onSelect(index);
+  }
+
   render() {
-    const { files, selectedFileIndex, onAdd } = this.props;
+    const { files, selectedFileIndex, onSelect } = this.props;
 
     return (
       <ul className="sidebar">
+        {files.map((file, index)=>{
+          return <SidebarItem key={index} file={file} onClick={this.handleClick.bind(null, index)} isSelected={selectedFileIndex === index}/>
+        })}
       </ul>
     );
   }
